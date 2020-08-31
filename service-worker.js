8ea9e8c09e893defa2a817150aa4dc587fbc5e50
sobchuksasha <sobchuksasha1995@gmail.com>
2020-08-31 20:44:47 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.342e87a9fff4b4cfafb26cc015f85197.js"
 );
 
 self.addEventListener('message', (event) => {
